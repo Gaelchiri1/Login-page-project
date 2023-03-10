776d83adeccd5e17da022769241994abdc2aeac9
Gaelchiri1 <mayrachiri12@gmail.com>
2023-03-10 16:54:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+const btnLogin = document.querySelector('.btnLogin');
+
+const iconClose = document.querySelector('.icon-close');
+
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnLogin.addEventListener('click', () => {
+        wrapper.classList.add('active-popup');
+    });
+
+    iconClose.addEventListener('click', ()=> {
+        wrapper.classList.remove('active-popup');
+    });
